b16ad52c5f3d54b8e95c839e493ed13dd29c2bef
Eiryn <eiryn@live.co.uk>
2024-02-13 15:08:04 +0000

    add unity

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y + 2.7f, target.transform.position.z);
+    }
+}
