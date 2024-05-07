c4508713ab47258e7bab02a24aa5086accf24f79
Eiryn <eiryn@live.co.uk>
2024-05-07 14:07:10 +0100

    basic t/f for anxiety

--- Assets/Scripts/EF_MannequinControls.cs
+++ Assets/Scripts/EF_MannequinControls.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class EF_MannequinControls : MonoBehaviour
 {
     public GameObject target;
+    public bool anxietyHigh = false;
+    //get anxiety script
 
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player");
+        anxietyHigh = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        var rotation = Quaternion.LookRotation(target.transform.position - transform.position);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 0.5f);
+        if (anxietyHigh == true)
+        {
+            var rotation = Quaternion.LookRotation(target.transform.position - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 0.5f);
+        }
     }
 }
